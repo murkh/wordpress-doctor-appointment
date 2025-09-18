06e371e53427330a095f6f921e7268d9c4b9265c
Gaurav Panchal <gkp.125@gmail.com>
2025-09-19 01:31:44 +0530

    Initialized

---
+++ doctor-appointment/doctor-appointment.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name: Doctor Appointment
+ * Description: A plugin for booking dental appointments.
+ * Version: 1.0
+ * Author: Gaurav Panchal 
+ */
+
+if (!defined("ABSPATH")) {
+    exit;
+}
+
